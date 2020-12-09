e089a0075554a0af371809e9e617f1aa97b5766c
Ruth Chavez <75191270@continental.edu.pe>
2020-12-09 05:57:30 -0500

    UI

--- NamePicker/Form1.Designer.cs
+++ NamePicker/Form1.Designer.cs
@@ -47,9 +47,11 @@ namespace NamePicker
             // 
             // enterName
             // 
+            this.enterName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.enterName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.enterName.Location = new System.Drawing.Point(12, 42);
             this.enterName.Name = "enterName";
-            this.enterName.Size = new System.Drawing.Size(171, 20);
+            this.enterName.Size = new System.Drawing.Size(245, 13);
             this.enterName.TabIndex = 0;
             this.enterName.TextChanged += new System.EventHandler(this.nameEntere_TextChanged);
             // 
@@ -65,30 +67,38 @@ namespace NamePicker
             // 
             // addName
             // 
+            this.addName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(54)))), ((int)(((byte)(96)))));
+            this.addName.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.addName.Location = new System.Drawing.Point(13, 69);
             this.addName.Name = "addName";
             this.addName.Size = new System.Drawing.Size(75, 23);
             this.addName.TabIndex = 2;
             this.addName.Text = "Agregar";
-            this.addName.UseVisualStyleBackColor = true;
+            this.addName.UseVisualStyleBackColor = false;
             this.addName.Click += new System.EventHandler(this.addName_Click);
             // 
             // removeName
             // 
-            this.removeName.Location = new System.Drawing.Point(108, 69);
+            this.removeName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(54)))), ((int)(((byte)(96)))));
+            this.removeName.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.removeName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.removeName.Location = new System.Drawing.Point(182, 69);
             this.removeName.Name = "removeName";
             this.removeName.Size = new System.Drawing.Size(75, 23);
             this.removeName.TabIndex = 3;
             this.removeName.Text = "Eliminar";
-            this.removeName.UseVisualStyleBackColor = true;
+            this.removeName.UseVisualStyleBackColor = false;
             this.removeName.Click += new System.EventHandler(this.removeName_Click);
             // 
             // selectedName
             // 
+            this.selectedName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.selectedName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.selectedName.FormattingEnabled = true;
-            this.selectedName.Location = new System.Drawing.Point(13, 123);
+            this.selectedName.Location = new System.Drawing.Point(13, 122);
             this.selectedName.Name = "selectedName";
-            this.selectedName.Size = new System.Drawing.Size(170, 121);
+            this.selectedName.Size = new System.Drawing.Size(244, 130);
             this.selectedName.TabIndex = 5;
             this.selectedName.SelectedIndexChanged += new System.EventHandler(this.selectedName_SelectedIndexChanged);
             // 
@@ -104,40 +114,51 @@ namespace NamePicker
             // 
             // selectName
             // 
-            this.selectName.Location = new System.Drawing.Point(13, 253);
+            this.selectName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(54)))), ((int)(((byte)(96)))));
+            this.selectName.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.selectName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.selectName.Location = new System.Drawing.Point(12, 263);
             this.selectName.Name = "selectName";
             this.selectName.Size = new System.Drawing.Size(75, 23);
             this.selectName.TabIndex = 7;
             this.selectName.Text = "Seleccionar";
-            this.selectName.UseVisualStyleBackColor = true;
+            this.selectName.UseVisualStyleBackColor = false;
             this.selectName.Click += new System.EventHandler(this.selectName_Click);
             // 
             // import
             // 
-            this.import.Location = new System.Drawing.Point(459, 225);
+            this.import.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(54)))), ((int)(((byte)(96)))));
+            this.import.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.import.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.import.Location = new System.Drawing.Point(523, 234);
             this.import.Name = "import";
             this.import.Size = new System.Drawing.Size(75, 23);
             this.import.TabIndex = 8;
             this.import.Text = "Importar";
-            this.import.UseVisualStyleBackColor = true;
+            this.import.UseVisualStyleBackColor = false;
             this.import.Click += new System.EventHandler(this.import_Click);
             // 
             // export
             // 
-            this.export.Location = new System.Drawing.Point(459, 254);
+            this.export.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(54)))), ((int)(((byte)(96)))));
+            this.export.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.export.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.export.Location = new System.Drawing.Point(523, 263);
             this.export.Name = "export";
             this.export.Size = new System.Drawing.Size(75, 23);
             this.export.TabIndex = 9;
             this.export.Text = "Exportar";
-            this.export.UseVisualStyleBackColor = true;
+            this.export.UseVisualStyleBackColor = false;
             this.export.Click += new System.EventHandler(this.export_Click);
             // 
             // namesEntered
             // 
+            this.namesEntered.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.namesEntered.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.namesEntered.FormattingEnabled = true;
-            this.namesEntered.Location = new System.Drawing.Point(198, 13);
+            this.namesEntered.Location = new System.Drawing.Point(273, 22);
             this.namesEntered.Name = "namesEntered";
-            this.namesEntered.Size = new System.Drawing.Size(244, 264);
+            this.namesEntered.Size = new System.Drawing.Size(244, 260);
             this.namesEntered.TabIndex = 4;
             this.namesEntered.SelectedIndexChanged += new System.EventHandler(this.namesEntered_SelectedIndexChanged);
             // 
@@ -145,7 +166,7 @@ namespace NamePicker
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label3.Location = new System.Drawing.Point(195, 284);
+            this.label3.Location = new System.Drawing.Point(10, 297);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(202, 13);
             this.label3.TabIndex = 10;
@@ -154,19 +175,23 @@ namespace NamePicker
             // 
             // clean
             // 
-            this.clean.Location = new System.Drawing.Point(391, 279);
+            this.clean.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(54)))), ((int)(((byte)(96)))));
+            this.clean.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clean.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.clean.Location = new System.Drawing.Point(452, 292);
             this.clean.Name = "clean";
             this.clean.Size = new System.Drawing.Size(51, 23);
             this.clean.TabIndex = 11;
             this.clean.Text = "Limpiar";
-            this.clean.UseVisualStyleBackColor = true;
+            this.clean.UseVisualStyleBackColor = false;
             this.clean.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(545, 311);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(609, 328);
             this.Controls.Add(this.clean);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.export);
@@ -179,6 +204,7 @@ namespace NamePicker
             this.Controls.Add(this.addName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.enterName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Name Picker";
